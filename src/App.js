fc46aeee05997e52ac46ffd7ce5556126e453cc7
jacobnathanson <jacobnath93@gmail.com>
2021-12-29 11:49:29 -0500

    browser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-     <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>hello</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
