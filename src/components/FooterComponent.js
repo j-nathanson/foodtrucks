62cda154c9affec6e71911e7cfdc22b5a1ebadbd
jacobnathanson <jacobnath93@gmail.com>
2021-12-30 13:19:23 -0500

    basic footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import "../styles/Footer.scss"
 
 function Footer() {
     return (
-        <div className="container-fluid bg-info">
-            <h1>Footer Goes Here</h1>
-        </div>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="col-6 col-sm-2 ">
+                            <h5>Links</h5>
+                            <ul class="list-unstyled">
+                                <li><Link to='/home'>Home</Link></li>
+                                <li><Link to='/catering'>Catering</Link></li>
+                                <li><Link to='/ourtrucks'>Our Trucks</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
