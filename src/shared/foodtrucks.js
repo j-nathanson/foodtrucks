afbb820bde62eee9e662f9e3c3b299b5876444fe
jacobnathanson <jacobnath93@gmail.com>
2022-01-02 18:44:19 -0500

    foodtruck data basic

--- src/shared/foodtrucks.js
+++ src/shared/foodtrucks.js
@@ -1,10 +1,52 @@
 const FOODTRUCKS = [
     {
-        id: 0,
+        id: 1,
         name: "Big D's Grub",
-        imgMain: "",
-        imgCard: "",
-        menu: "",
-        rating: ""
-    }
+        imgMain: "/assets/images/big-d-1.jpeg",
+        imgCard: "/assets/images/big-d-2.jpeg",
+        menu: "http://www.bigdsgrub.com/menu/",
+        rating: 4
+    },
+    {
+        id: 2,
+        name: "Tacos El Poblanito",
+        imgMain: "/assets/images/poblanito-1.jpeg",
+        imgCard: "/assets/images/poblanito-2.jpeg",
+        menu: "https://www.seamless.com/menu/tacos-el-poblanito-2-4405-new-utrecht-ave-brooklyn/1937278",
+        rating: 4
+    },
+    {
+        id: 3,
+        name: "Stuf'd Truck",
+        imgMain: "/assets/images/stuftd-1.jpeg",
+        imgCard: "/assets/images/stuftd-2.jpeg",
+        menu: "https://www.stufdtruck.com/menus",
+        rating: 4
+    },
+    {
+        id: 4,
+        name: "Tacos El Bronco",
+        imgMain: "/assets/images/el-bronco-1.jpeg",
+        imgCard: "/assets/images/el-bronco-2.jpeg",
+        menu: "http://places.singleplatform.com/tacos-el-bronco-restaurant/menu",
+        rating: 4
+    },
+    {
+        id: 5,
+        name: "Poke Motion",
+        imgMain: "/assets/images/poke-motion-1.jpeg",
+        imgCard: "/assets/images/poke-motion-2.jpeg",
+        menu: "https://www.pokemotionfoodtruck.com/our-bowls-1",
+        rating: 4
+    },
+    {
+        id: 6,
+        name: "Dirty Bird",
+        imgMain: "/assets/images/dirty-bird-1.jpeg",
+        imgCard: "/assets/images/dirty-bird-2.jpeg",
+        menu: "https://www.menupix.com/menudirectory/menu.php?id=28175731",
+        rating: 4
+    },
 ]
+
+export default FOODTRUCKS;
