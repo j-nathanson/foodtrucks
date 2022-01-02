e2c417a6b343323167dd39925819749e3b2de93a
jacobnathanson <jacobnath93@gmail.com>
2022-01-02 17:46:56 -0500

    img-text-left and scss restructure

--- src/components/pages/HomePageComponent.js
+++ src/components/pages/HomePageComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Jumbotron, Button, Container } from "reactstrap";
+import { Jumbotron, Button, Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+
 
 function HomePage() {
     return (
@@ -13,7 +14,26 @@ function HomePage() {
                     </Link>
                 </Container>
             </Jumbotron>
-            <h3>Image Text Left</h3>
+            <Container fluid className="img-text-left p-4">
+                <Row className="justify-content-center">
+                    <h2>Who We Are</h2>
+                </Row>
+                <Row className="align-items-center">
+                    <Col sm="7" className=" order-sm-last">
+                        <img src='assets/images/hamburger.jpg' alt="hamburger" className="img-fluid img-thumbnail" />
+                    </Col>
+                    <Col sm="5">
+                        <p>"We are foodies with a mission of connecting New Yorkers with the hidden gems offered
+                            on
+                            four wheels.
+                            Whether you're looking for a quick bite or need your next event planned, we got you covered"
+                        </p>
+                    </Col>
+
+                </Row>
+
+
+            </Container>
             <h3>Card Gallery img only</h3>
             <h3>Image Text Right</h3>
         </div>
