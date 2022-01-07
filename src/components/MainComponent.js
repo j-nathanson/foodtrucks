86aa3ce9101d16740d8a30c3aeea078311b81ec8
jacobnathanson <jacobnath93@gmail.com>
2022-01-07 17:44:34 -0500

    scroll to top

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePageComponent';
 import CateringPage from './pages/CateringPageComponent';
 import TrucksPage from './pages/TrucksPageComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import FOODTRUCKS from '../shared/foodtrucks';
 import TEXTOBJECTS from '../shared/textobjects';
 import CAROUSEL_ITEMS from '../shared/carousel';
@@ -23,8 +24,8 @@ class Main extends Component {
         return (
             <div>
                 <Navigation />
+                <ScrollToTop />
                 <Switch>
-
                     <Route path='/home' render={() => <HomePage foodTrucks={this.state.foodTrucks} textObjects={this.state.textObjects} />} />
                     <Route path='/catering' render={() => <CateringPage carouselItems={this.state.carouselItems} textObjects={this.state.textObjects} />} />
                     <Route path='/ourtrucks' render={() => <TrucksPage foodTrucks={this.state.foodTrucks} textObjects={this.state.textObjects} />} />
