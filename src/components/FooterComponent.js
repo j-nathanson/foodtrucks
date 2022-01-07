98062194cecac3452be7ef1ce7de0e829e0582d0
jacobnathanson <jacobnath93@gmail.com>
2022-01-07 17:22:36 -0500

    media query for footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+//  <div className="col-4 col-sm-6">
+//     <h5>Links</h5>
+//     <ul className="list-unstyled">
+//         <li><Link to='/home'>Home</Link></li>
+//         <li><Link to='/catering'>Catering</Link></li>
+//         <li><Link to='/ourtrucks'>Our Trucks</Link></li>
+//     </ul>
+// </div>
+
+// <div className="col-8 col-sm-6">
+//     <div className="text-center mb-3">
 
 function Footer() {
     return (
         <footer>
             <div className="container">
                 <div className="row">
-                    <div className="col-4 col-sm-6">
+                    <div className="col-4 col-sm-6 links-container">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to='/home'>Home</Link></li>
@@ -15,7 +26,7 @@ function Footer() {
                         </ul>
                     </div>
 
-                    <div className="col-8 col-sm-6">
+                    <div className="col-8 col-sm-6 mx-auto">
                         <div className="text-center mb-3">
                             <h5>Social</h5>
                         </div>
