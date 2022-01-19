72ba5dbb8dcf1377387f6810ec4cb6e8ee3d366c
jacobnathanson <jacobnath93@gmail.com>
2022-01-19 16:58:15 -0500

    setChoice action creator

--- src/redux/foodTruckSlice.js
+++ src/redux/foodTruckSlice.js
@@ -2,14 +2,24 @@ import { createSlice } from '@reduxjs/toolkit';
 import FOOD_TRUCKS from '../shared/foodTrucks';
 
 const initialState = {
-    foodTrucks: FOOD_TRUCKS
+    foodTrucks: FOOD_TRUCKS,
+    choice: "everything"
 }
 
 export const foodTruckSlice = createSlice({
     name: 'foodTrucks',
     initialState,
-    reducers: {}
+    reducers: {
+        setChoice: (state, action) => {
+            if (action.payload === "everything") {
+                state.foodTrucks = initialState.foodTrucks
+            } else {
+                state.foodTrucks = state.foodTrucks.filter(foodTruck => foodTruck.cuisine === action.payload)
+            }
+        }
+    }
 })
 
+export const { setChoice } = foodTruckSlice.actions
 
 export default foodTruckSlice.reducer;
