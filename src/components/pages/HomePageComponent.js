ed7b2e906fe7b0c7976767277f4c5a5531c6f5c2
jacobnathanson <jacobnath93@gmail.com>
2022-01-19 16:50:32 -0500

    change route path in main

--- src/components/pages/HomePageComponent.js
+++ src/components/pages/HomePageComponent.js
@@ -4,7 +4,7 @@ import ImageTextBox from "../ImageTextBoxComponent";
 import ImageGallery from "../ImageGalleryComponent";
 import { useSelector } from 'react-redux';
 
-function HomePage(props) {
+function HomePage() {
     const textObjectTop = useSelector(state => state.textObjectReducer.textObjects[0]);
     const textObjectBottom = useSelector(state => state.textObjectReducer.textObjects[1]);
   
