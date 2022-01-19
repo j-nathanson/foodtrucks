6b3309a24f8f95a6ffdea915bc9fe289a77de16e
jacobnathanson <jacobnath93@gmail.com>
2022-01-19 17:20:23 -0500

    convert main into functional component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navigation from './NavigationComponent';
 import Footer from './FooterComponent';
 import HomePage from './pages/HomePageComponent';
@@ -6,35 +6,23 @@ import CateringPage from './pages/CateringPageComponent';
 import TrucksPage from './pages/TrucksPageComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
-import FOOD_TRUCKS from '../shared/foodTrucks';
-import TEXT_OBJECTS from '../shared/textObjects';
 
 // state data passed through Main
-class Main extends Component {
-    constructor(props) {
-        super(props);
+function Main() {
 
-        this.state = {
-            foodTrucks: FOOD_TRUCKS,
-            textObjects: TEXT_OBJECTS,
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Navigation />
-                <ScrollToTop />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route path='/catering' component={CateringPage} />
-                    <Route path='/ourtrucks' render={() => <TrucksPage foodTrucks={this.state.foodTrucks} textObjects={this.state.textObjects} />} />
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Navigation />
+            <ScrollToTop />
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route path='/catering' component={CateringPage} />
+                <Route path='/ourtrucks' component={TrucksPage} />
+                <Redirect to='/home' />
+            </Switch>
+            <Footer />
+        </div>
+    )
 }
 
 export default Main;
--- src/components/pages/TrucksPageComponent.js
+++ src/components/pages/TrucksPageComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Col, Container, Row } from "reactstrap";
 import ImageTextBox from "../ImageTextBoxComponent";
 import TruckInfoCard from "../TruckInfoCardComponent";
