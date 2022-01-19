0b002a9bc64d389ee451b2a019c851e47a3e74bf
jacobnathanson <jacobnath93@gmail.com>
2022-01-19 17:28:46 -0500

    convert nav to functional component

--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -1,48 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from "reactstrap";
 import { NavLink } from 'react-router-dom';
 import logo from "../assets/images/logo.svg"
 
-class Navigation extends Component {
-    constructor(props) {
-        super(props);
+function Navigation() {
+    const [isNavOpen, setNav] = useState(false);
 
-        this.state = {
-            isNavOpen: false
-        };
+    const toggleNav = () => {
+        setNav(!isNavOpen);
     }
-    toggleNav = () => {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
-    render() {
-        return (
-            <Navbar dark sticky="top" expand="md">
-                <Container>
-                    <NavbarBrand className="mr-auto" href="/">
-                        <img src={logo} alt="Logo" className="mr-2" width={40} height={40} />
-                        Foodies Trucks NYC
-                    </NavbarBrand>
 
-                    <NavbarToggler onClick={this.toggleNav} />
-                    <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar className="ml-auto">
-                            <NavItem>
-                                <NavLink className='nav-link' to='/home'>Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='nav-link' to='/catering'>Catering</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='nav-link' to='/ourtrucks'>Our Trucks</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar >
-        );
-    }
+    return (
+        <Navbar dark sticky="top" expand="md">
+            <Container>
+                <NavbarBrand className="mr-auto" href="/">
+                    <img src={logo} alt="Logo" className="mr-2" width={40} height={40} />
+                    Foodies Trucks NYC
+                </NavbarBrand>
+                <NavbarToggler onClick={toggleNav} />
+                <Collapse isOpen={isNavOpen} navbar>
+                    <Nav navbar className="ml-auto">
+                        <NavItem>
+                            <NavLink className='nav-link' to='/home'>Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/catering'>Catering</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/ourtrucks'>Our Trucks</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default Navigation;
