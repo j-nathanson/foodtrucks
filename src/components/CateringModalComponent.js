b6a2beb50e3565a3113656f30cd43474b6e488f5
jacobnathanson <jacobnath93@gmail.com>
2022-01-18 20:50:06 -0500

    finish functional component and delete Class component

--- src/components/CateringModalComponent.js
+++ src/components/CateringModalComponent.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, {  useState } from "react";
 import { Button, Col, Container, Label, Modal, ModalHeader, ModalBody, Row } from "reactstrap";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
@@ -13,6 +13,10 @@ function CateringModal() {
     const toggleModal = () => {
         setModal(!isModalOpen);
     }
+    const handleSubmit = (values) => {
+        toggleModal();
+        console.log(values);
+    }
     return (
         <Container>
             <Row className="justify-content-center toggle-modal-btn">
@@ -24,106 +28,67 @@ function CateringModal() {
                 <ModalHeader toggle={() => toggleModal()}>
                     Book a truck
                 </ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => handleSubmit(values)} className="container">
+                        <div className="form-group">
+                            <Label htmlFor="name">Your Name</Label>
+                            <Control.text model=".name" id="name" name="name" className='form-control' placeholder="John Truckman" />
+                        </div>
+                        <div className="form-group">
+                            <Label htmlFor="email">Your Name</Label>
+                            <Control.text type="email" model=".email" id="email" name="email" className='form-control' placeholder="gyroking99@gmail.com" />
+                        </div>
+                        <div className="form-group">
+                            <Label htmlFor="date">Date of Event</Label>
+                            <Control.text type="date" model=".date" id="date" name="date" className='form-control' />
+                        </div>
+                        <div className="form-group">
+                            <div className="row justify-content-center">
+                                <Label>How many guest are you expecting?</Label>
+                            </div>
+                            <Row>
+                                <Col>
+                                    <Row className="justify-content-center">
+                                        <Label htmlFor="lessThan100">50-200</Label>
+                                    </Row>
+                                    <Control.radio model=".guests" id="lessThan100" name="guests" value="small" className='form-control' />
+                                </Col>
+                                <Col>
+                                    <Row className="justify-content-center">
+                                        <Label htmlFor="medium">200-350</Label>
+                                    </Row>
+                                    <Control.radio model=".guests" id="medium" name="guests" value="medium" className='form-control' />
+                                </Col>
+                                <Col>
+                                    <Row className="justify-content-center">
+                                        <Label htmlFor="large">350+</Label>
+                                    </Row>
+                                    <Control.radio model=".guests" id="large" name="guests" value="large" className='form-control' />
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className="form-group">
+                            <Label htmlFor="foodTrucks">Select your favorite truck</Label>
+                            <Control.select model=".foodTrucks" id="foodTrucks" name="foodTrucks" className='form-control'>
+                                <option value="none" selected disabled hidden>Select an Option</option>
+                                <option value="Big D's Grub">Big D's Grub</option>
+                                <option value="Tacos El Poblanito">Tacos El Poblanito</option>
+                                <option value="Stuf'd Truck">Stuf'd Truck</option>
+                                <option value="Tacos El Bronco">Tacos El Bronco</option>
+                                <option value="Poke Motion">Poke Motion</option>
+                                <option value="Dirty Bird">Dirty Bird</option>
+                            </Control.select>
+                        </div>
+                        <div className="form-group">
+                            <Label htmlFor="text">Any additional info?</Label>
+                            <Control.textarea model=".text" id="text" name="text" className="form-control" rows={6} />
+                        </div>
+                        <Button type="submit" value="submit" color="info">Submit</Button>
+                    </LocalForm>
+                </ModalBody>
             </Modal>
-
         </Container>
     )
-}
-class CateringModal2 extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isModalOpen: false
-        }
-    }
-
-    toggleModal = () => {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        })
-    }
-    handleSubmit = (values) => {
-        this.toggleModal();
-        console.log(values);
-
-    }
-
-    render() {
-        return (
-            <Container>
-                <Row className="justify-content-center toggle-modal-btn">
-                    <Button onClick={this.toggleModal} color="info" size="lg" className="mb-3">
-                        Start Here
-                    </Button>
-                </Row>
-
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>
-                        Book a truck
-                    </ModalHeader>
-                    <ModalBody>
-                        <LocalForm onSubmit={values => this.handleSubmit(values)} className="container">
-                            <div className="form-group">
-                                <Label htmlFor="name">Your Name</Label>
-                                <Control.text model=".name" id="name" name="name" className='form-control' placeholder="John Truckman" />
-                            </div>
-                            <div className="form-group">
-                                <Label htmlFor="email">Your Name</Label>
-                                <Control.text type="email" model=".email" id="email" name="email" className='form-control' placeholder="gyroking99@gmail.com" />
-                            </div>
-                            <div className="form-group">
-                                <Label htmlFor="date">Date of Event</Label>
-                                <Control.text type="date" model=".date" id="date" name="date" className='form-control' />
-                            </div>
-                            <div className="form-group">
-                                <div className="row justify-content-center">
-                                    <Label>How many guest are you expecting?</Label>
-                                </div>
-                                <Row>
-                                    <Col>
-                                        <Row className="justify-content-center">
-                                            <Label htmlFor="lessThan100">50-200</Label>
-                                        </Row>
-                                        <Control.radio model=".guests" id="lessThan100" name="guests" value="small" className='form-control' />
-                                    </Col>
-                                    <Col>
-                                        <Row className="justify-content-center">
-                                            <Label htmlFor="medium">200-350</Label>
-                                        </Row>
-                                        <Control.radio model=".guests" id="medium" name="guests" value="medium" className='form-control' />
-                                    </Col>
-                                    <Col>
-                                        <Row className="justify-content-center">
-                                            <Label htmlFor="large">350+</Label>
-                                        </Row>
-                                        <Control.radio model=".guests" id="large" name="guests" value="large" className='form-control' />
-                                    </Col>
-                                </Row>
-                            </div>
-                            <div className="form-group">
-                                <Label htmlFor="foodTrucks">Select your favorite truck</Label>
-                                <Control.select model=".foodTrucks" id="foodTrucks" name="foodTrucks" className='form-control'>
-                                    <option value="none" selected disabled hidden>Select an Option</option>
-                                    <option value="Big D's Grub">Big D's Grub</option>
-                                    <option value="Tacos El Poblanito">Tacos El Poblanito</option>
-                                    <option value="Stuf'd Truck">Stuf'd Truck</option>
-                                    <option value="Tacos El Bronco">Tacos El Bronco</option>
-                                    <option value="Poke Motion">Poke Motion</option>
-                                    <option value="Dirty Bird">Dirty Bird</option>
-                                </Control.select>
-                            </div>
-                            <div className="form-group">
-                                <Label htmlFor="text">Any additional info?</Label>
-                                <Control.textarea model=".text" id="text" name="text" className="form-control" rows={6} />
-                            </div>
-                            <Button type="submit" value="submit" color="info">Submit</Button>
-                        </LocalForm>
-                    </ModalBody>
-                </Modal>
-            </Container>
-        )
-    }
 }
 
 export default CateringModal;
