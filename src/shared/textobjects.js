c457a5d6a0e36b414978730a5623009233f4df0f
jacobnathanson <jacobnath93@gmail.com>
2022-01-21 16:19:19 -0500

    renamed shared foodTrucks file

--- src/shared/textobjects.js
+++ src/shared/textobjects.js
@@ -3,49 +3,49 @@ const TEXT_OBJECTS = [
         id: 0,
         heading: "Who We Are",
         img: "assets/images/hamburger.jpg",
-        alt:"Hamburger",
+        alt: "Hamburger",
         imgOrder: "order-sm-last",
         style: "img-text-left",
         description: "\"We are foodies with a mission of connecting New Yorkers with the hidden gems offered on four wheels. Whether you're looking for a quick bite or need your next event planned, we got you covered.\"",
         showButton: "d-none",
         buttonText: "",
-        buttonLink:""
+        buttonLink: ""
     },
     {
         id: 1,
         heading: "Want Catering?",
         img: "assets/images/catering-1.jpeg",
-        alt:"Hamburger",
+        alt: "Hamburger",
         imgOrder: "order-sm-first",
         style: "img-text-right",
         description: "Our trucks are available to bring the best food to your next event. Click below to find out more!",
         showButton: "btn-info btn-lg",
         buttonText: "Start Here",
-        buttonLink:"/catering"
+        buttonLink: "/catering"
     },
     {
         id: 2,
         heading: "We do Catering!",
         img: "assets/images/catering-2.jpeg",
-        alt:"Hamburger",
+        alt: "Hamburger",
         imgOrder: "order-sm-first",
         style: "img-text-right",
         description: "Our trucks are available to bring the best food to your next event. Click below to find out more!",
         showButton: "btn-info btn-lg",
         buttonText: "Start Here",
-        buttonLink:"/catering"
+        buttonLink: "/catering"
     },
     {
         id: 3,
         heading: "Imagine the smiles at your next event!",
         img: "assets/images/food-truck-line.jpg",
-        alt:"Hamburger",
+        alt: "Hamburger",
         imgOrder: "order-sm-last",
         style: "img-text-left-switch",
         description: "Any party, and any budget! Checkout below to get an estimate on your next great event.",
         showButton: "d-none",
         buttonText: "",
-        buttonLink:""
+        buttonLink: ""
     },
 
 ]
