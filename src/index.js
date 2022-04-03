42116ce7e85dac92f86e00efff471b2da6ef9691
jacobnathanson <jacobnath93@gmail.com>
2022-04-03 13:40:58 -0400

    remove web vital from index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
@@ -20,4 +19,4 @@ ReactDOM.render(
 );
 
 
-reportWebVitals();
+
